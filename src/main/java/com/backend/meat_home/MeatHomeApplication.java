bc2afdda1decf46b8e2cb66ed2c402046fe5874a
Daniel-Sameh <111789605+Daniel-Sameh@users.noreply.github.com>
2025-08-21 01:32:50 +0300

    Initial Commit

---
+++ src/main/java/com/backend/meat_home/MeatHomeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.meat_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/meat_home/MeatHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.meat_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
