35d504c0ae55c163b9e0f98c94acd688a4ee617b
Daniel-Sameh <111789605+Daniel-Sameh@users.noreply.github.com>
2025-08-21 17:03:11 +0300

    Configured the dotenv

--- src/main/java/com/backend/meat_home/MeatHomeApplication.java
+++ src/main/java/com/backend/meat_home/MeatHomeApplication.java
@@ -2,9 +2,19 @@ package com.backend.meat_home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MeatHomeApplication {
+
+	static {
+		Dotenv.configure()
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeatHomeApplication.class, args);
