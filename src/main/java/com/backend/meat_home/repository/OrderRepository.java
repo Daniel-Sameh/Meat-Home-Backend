ff4f13d46bc75873fbcabdd9eaa1d793e0ee71cc
toqa <toqaashraff35@gmail.com>
2025-08-22 22:47:05 +0300

    edit order- repository

--- src/main/java/com/backend/meat_home/repository/OrderRepository.java
+++ src/main/java/com/backend/meat_home/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.backend.meat_home.repository;
 
 import com.backend.meat_home.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByStatus(String status);
 }
