cb298c4d61268638792788a26438944a1cc411c6
Mohamed Khamis <mohamedkhamis20045@gmail.com>
2025-08-23 13:15:11 +0300

    feat: add basic security configuration

--- src/main/java/com/backend/meat_home/config/SecurityConfig.java
+++ src/main/java/com/backend/meat_home/config/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.backend.meat_home.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("api/auth/register", "api/auth/login").permitAll() // public endpoints
+                        .anyRequest().authenticated() // everything else requires authentication
+                );
+
+        return http.build();
+    }
 }
